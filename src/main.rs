302b04078184c525fdab806b051a6e293d027a55
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2023-03-18 23:58:33 +0100

    `rustc` compiles `VMDK.rs` (from `vbox/src/VBox/Storage/VMDK.cpp`) without errors

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
 #![feature(try_blocks)]
-use std::collections::HashSet;
-use std::env;
+
+use std::collections::{HashMap, HashSet};
+use std::{env, path};
+use std::fmt::{Debug, Formatter};
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
+
 //use std::process::exit;
-use clang::{Clang, Index, EntityKind, Entity};
+use clang::{Clang, Entity, EntityKind, Index, TypeKind};
 use clang::source::Location;
+
 //use clib;
 use regex::Regex;
 
-type Dictionary = HashSet<String>;
-
-struct KnownTypes {
-    structs: Dictionary,
-}
-
-impl KnownTypes {
-    fn new() -> Self {
-        KnownTypes {
-            structs: Dictionary::new(),
-        }
-    }
-
-    fn add_struct(&mut self, name: &str) {
-        self.structs.insert(name.to_string());
-    }
-}
-
 struct Field {
     name: String,
-    type_ : String,
+    type_: String,
 }
 
 struct StructGen {
@@ -53,14 +40,82 @@ impl StructGen {
     }
 
     fn get_rust_code(&self) -> String {
-        let mut code = format!("#[derive(Debug, Copy, Clone)]\n#[repr(C)]\nstruct {} {{\n", self.name);
+        let mut code = format!("\n/*#[derive(Debug, Copy, Clone)]*/\n#[repr(C)]\nstruct {} {{\n", self.name);
         self.members
             .iter()
             .for_each(|fld| {
                 code += format!("  {}: {},\n", fld.name, fld.type_).as_str();
-            } );
-        code += "}\n\n";
+            });
+        code += "}\n";
         code
+    }
+}
+
+#[derive(Debug)]
+struct TypeDef {
+    name: String,
+    kind: TypeKind,
+    def: String,
+}
+
+type StrSet = HashSet<String>;
+type Dictionary<T> = HashMap<String, T>;
+type TypeDefDict = Dictionary<TypeDef>;
+type EnumDefDict = Dictionary<Vec<String>>;
+
+#[derive(Debug, PartialEq)]
+enum KnownType {
+    None,
+    Struct,
+    TypeDef,
+    Enum,
+}
+
+struct KnownTypes {
+    structs: StrSet,
+    typedefs: TypeDefDict,
+    enums: EnumDefDict,
+}
+
+impl KnownTypes {
+    fn new() -> Self {
+        KnownTypes {
+            structs: StrSet::new(),
+            typedefs: TypeDefDict::new(),
+            enums: EnumDefDict::new(),
+        }
+    }
+
+    fn is_know_type(&self, name: &str) -> KnownType {
+        if self.structs.contains(name) {
+            return KnownType::Struct;
+        }
+        if self.typedefs.contains_key(name) {
+            return KnownType::TypeDef;
+        }
+        if self.enums.contains_key(name) {
+            return KnownType::Enum;
+        }
+
+        KnownType::None
+    }
+
+    fn add_struct(&mut self, name: &str) {
+        self.structs.insert(name.to_string());
+    }
+
+    fn add_typedef(&mut self, td: TypeDef) {
+        self.typedefs.insert(td.name.clone(), td);
+    }
+
+    fn add_enum(&mut self, name: &str, values: Vec<String>) {
+        self.enums.insert(name.to_string(), values);
+    }
+}
+
+impl Debug for KnownTypes {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "structs: {:?}\ntypedefs: {:?}\nenums: {:?}", self.structs, self.typedefs, self.enums)
     }
 }
 
@@ -88,8 +143,8 @@ impl<'tu> Converter<'tu> {
 
     fn try_c_to_rust_type(&mut self, c_type: &str) -> Option<String> {
         lazy_static::lazy_static! {
-        static ref RE_ARRAY: Regex = Regex::new(r"([^\[]+)\[([^\]]*)\]").unwrap();
-    }
+            static ref RE_ARRAY: Regex = Regex::new(r"([^\[]+)\[([^\]]*)\]").unwrap();
+        }
 
         if let Some(cap) = RE_ARRAY.captures(c_type) {
             let arr_type = cap.get(1).unwrap().as_str().trim();
@@ -100,8 +155,13 @@ impl<'tu> Converter<'tu> {
             "int" => "c_int",
             "bool" => "bool",
             "char" => "c_char",
-            "const char *" => "CStr",
+            "const char *" => "Box<CStr>",
             "unsigned int" => "c_uint",
+            "size_t" => "usize",
+            "int *" => "*const c_int",
+            "char *" => "*const c_char",
+            "char **" => "*const *const c_char",
+            "void *" => "c_void",
             _ => return None,
         }.to_string())
     }
@@ -110,12 +170,40 @@ impl<'tu> Converter<'tu> {
         if let Some(s) = self.try_c_to_rust_type(c_type) {
             return s;
         }
+
+        match self.known_types.is_know_type(c_type) {
+            KnownType::Struct =>
+                return c_type.to_string(),
+            KnownType::TypeDef =>
+                return c_type.to_string(),
+            KnownType::Enum =>
+                return c_type.to_string(),
+            KnownType::None => {},
+        }
+
+        lazy_static::lazy_static! {
+            static ref RE_STRUCT_REF: Regex = Regex::new(r"struct ([^ ]+) *").unwrap();
+        }
+
+        if let Some(cap) = RE_STRUCT_REF.captures(c_type) {
+            let struct_name = cap.get(1).unwrap().as_str().trim();
+            return format!("*const {struct_name}");
+        }
+
         eprintln!("\x1B[31mmissed '{c_type}' line: {}, column: {}\x1b[0m", self.location.line, self.location.column);
         format!("!!!{c_type}!!!")
     }
 
     fn add_struct(&mut self, name: &str) {
         self.known_types.add_struct(name);
+    }
+
+    fn add_typedef(&mut self, td: TypeDef) {
+        self.known_types.add_typedef(td);
+    }
+
+    fn add_enum(&mut self, name: &str, values: Vec<String>) {
+        self.known_types.add_enum(name, values);
     }
 }
 
@@ -128,8 +216,9 @@ fn get_type(ent: &Entity) -> String {
 }
 
 fn main() {
-    let cpp_file = env::args().skip(1).next().unwrap().as_str().to_string();
-    println!("cpp_file: '{cpp_file}'");
+    let arg = env::args().skip(1).next().unwrap().as_str().to_string();
+    let cpp_file = path::Path::new(&arg);
+    println!("cpp_file: '{}'", cpp_file.display());
     let clang = Clang::new().unwrap();
     let index = Index::new(&clang, false, false);
     let arguments = vec!["-I", "dummy"];
@@ -141,49 +230,78 @@ fn main() {
     };
 
     let entity = tu.get_entity();
-    println!("{entity:?}");
+    //println!("{entity:?}");
 
     let mut rust_code =
-r"
-#![allow(dead_code, non_snake_case)]
-use std::ffi::{CStr, c_char, c_int};
+        r"
+#![allow(dead_code, non_snake_case, non_camel_case_types)]
+use std::ffi::{CStr, c_char, c_int, c_uint, c_void};
 
 fn main () {
 }
 
 ".to_string();
 
-    let mut output = File::create("./misc/src/lib.rs").unwrap();
+    let mut outfile = PathBuf::from(cpp_file.file_name().unwrap().to_str().unwrap());
+    outfile.set_extension("rs");
+    println!("rust_file: '{}'", outfile.canonicalize().unwrap().display());
+    let mut output = File::create(outfile).unwrap();
     let mut converter = Converter::new();
 
     for child in entity.get_children() {
-        match child.get_kind()  {
+        match child.get_kind() {
             EntityKind::StructDecl => {
-                converter.set_location(&child);
-                let name = get_name(&child);
-                let mut  str_def = StructGen::new(&name);
-                //println!("  StructDecl {}: ", name);
+                if child.get_children().len() > 0 {
+                    converter.set_location(&child);
+                    let name = get_name(&child);
+                    let mut str_def = StructGen::new(&name);
 
-                for field in child.get_children() {
-                    converter.set_location(&field);
-                    let fld_name = get_name(&field);
-                    let fld_type = get_type(&field);
+                    //println!("StructDecl: {name}");
+                    for field in child.get_children() {
+                        converter.set_location(&field);
+                        let fld_name = get_name(&field);
+                        let fld_type = get_type(&field);
 
-                    str_def.add_field(&fld_name, &converter.c_to_rust_type(&fld_type));
-                    //println!("    {}: {}", fld_name, fld_type);
+                        str_def.add_field(&fld_name, &converter.c_to_rust_type(&fld_type));
+                    }
+                    let def = str_def.get_rust_code();
+                    rust_code += &def;
+                    converter.add_struct(&name);
                 }
-                let def = str_def.get_rust_code();
-                //println!("{}", def);
-                rust_code += &def;
-                converter.add_struct(&name);
-                //println!("{rust_code}");
-                // exit(0);
-            },
+            }
+            EntityKind::TypedefDecl => {
+                let name = get_name(&child);
+                let td_type = &child.get_typedef_underlying_type().unwrap();
+                if converter.known_types.is_know_type(&name) == KnownType::None {
+                    let mut def_text = td_type.get_display_name().replace("struct ", "");
+
+                    //println!("TypedefDecl: {name}");
+                    if (name != def_text) && !def_text.starts_with("enum") {
+                        if def_text.ends_with("*") {
+                            def_text = format!("*const {}", def_text.trim_end_matches(|c| c=='*'));
+                        }
+                        rust_code += &format!("type {} = {};\n", name, def_text);
+                        converter.add_typedef(TypeDef{name: name, kind: td_type.get_kind(), def: def_text });
+                    }
+                }
+            }
+            EntityKind::EnumDecl => {
+                let name = get_name(&child);
+                let mut values = Vec::<String>::with_capacity(child.get_children().len());
+
+                //println!("EnumDecl: {name}");
+                for val in child.get_children() {
+                    values.push(get_name(&val));
+                }
+                rust_code += &format!("enum {name} {{\n    {}\n}}", values.join(",\n    "));
+                converter.add_enum(&name, values);
+            }
             _ => {
-                println!("  {child:?}");
-            },
+                //println!("  {child:?}");
+            }
         }
     }
 
+    //println!("\nknown_types:{:?}", converter.known_types);
     write!(output, "{rust_code}").unwrap();
 }
